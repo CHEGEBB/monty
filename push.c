de7ed3eaf992779fc5d5a7ab552a246c9e25163e
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 20:13:59 +0300

    Update push.c

--- push.c
+++ push.c
@@ -12,39 +12,39 @@
  */
 void f_push(stack_t **stack_head, unsigned int line_number)
 {
-	int value, index = 0, error_flag = 0;
+    int value, index = 0, error_flag = 0;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			index++;
-		for (; bus.arg[index] != '\0'; index++)
-		{
-			if (bus.arg[index] > '9' || bus.arg[index] < '0')
-				error_flag = 1;
-		}
-		if (error_flag == 1)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*stack_head);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*stack_head);
-		exit(EXIT_FAILURE);
-	}
+    if (bus.arg)
+    {
+        if (bus.arg[0] == '-')
+            index++;
+        for (; bus.arg[index] != '\0'; index++)
+        {
+            if (bus.arg[index] > '9' || bus.arg[index] < '0')
+                error_flag = 1;
+        }
+        if (error_flag == 1)
+        {
+            fprintf(stderr, "L%d: usage: push integer\n", line_number);
+            fclose(bus.file);
+            free(bus.content);
+            free_stack(*stack_head);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*stack_head);
+        exit(EXIT_FAILURE);
+    }
 
-	value = atoi(bus.arg);
+    value = atoi(bus.arg);
 
-	if (bus.lifi == 0)
-		addnode(stack_head, value);
-	else
-		addqueue(stack_head, value);
+    if (bus.lifi == 0)
+        addnode(stack_head, value);
+    else
+        addqueue(stack_head, value);
 }
