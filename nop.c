f11c377e6c60687def4abfd7d578547d51af882e
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 22:07:09 +0300

    Update nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_nop - Does nothing.
+ * @head: Double pointer to the beginning of the stack.
+ * @counter: Line number in the Monty file.
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	/* Does nothing */
+}
