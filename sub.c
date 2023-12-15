c226baf0afed735889668dc3e3d6409dda7fda3e
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 23:04:56 +0300

    Update sub.c

--- sub.c
+++ sub.c
@@ -6,7 +6,7 @@
  * @line_number: line number in the script
  * Return: no return
  */
-void pop_top(stack_t **stack_head, unsigned int line_number)
+int pop_top(stack_t **stack_head, unsigned int line_number)
 {
     stack_t *temp;
 
