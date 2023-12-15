001a159c9b7295af46a62a95040d8c4cf9a0e9a6
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-16 00:03:00 +0300

    Update div.c

--- div.c
+++ div.c
@@ -1,40 +1,65 @@
 #include "monty.h"
+
 /**
- * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+ * f_div - Divides the second top element by the top element of the stack
+ * @head: Pointer to the stack's head
+ * @counter: Current line number in the script
+ * Return: No return value
+ *
+ * Description:
+ * - This function divides the second top element by the top element of the stack.
+ * - It checks if the stack contains at least two elements before performing the division.
+ * - If the stack has fewer than two elements, an error message is displayed, and the program exits.
+ * - The line number of the script where the error occurred is also indicated.
+ * - If the top element is 0, a division by zero error is displayed, and the program exits.
+ * - The result of the division is stored in the second top element of the stack.
+ * - The top element is removed from the stack after the division.
+ */
 void f_div(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	int len = 0, aux;
+    stack_t *temp;
+    int num_nodes = 0, result;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+    temp = *head;
+
+    /* Count the number of nodes in the stack */
+    while (temp)
+    {
+        temp = temp->next;
+        num_nodes++;
+    }
+
+    /* Check if there are at least two elements in the stack */
+    if (num_nodes < 2)
+    {
+        /* Display an error message and terminate if the stack is too short */
+        fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    temp = *head;
+
+    /* Check for division by zero */
+    if (temp->n == 0)
+    {
+        /* Display an error message and terminate if division by zero is attempted */
+        fprintf(stderr, "L%d: division by zero\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Perform division and store the result in the second top element */
+    result = temp->next->n / temp->n;
+    temp->next->n = result;
+
+    /* Update the stack head to the second top element */
+    *head = temp->next;
+
+    /* Free the memory of the removed top element */
+    free(temp);
 }
