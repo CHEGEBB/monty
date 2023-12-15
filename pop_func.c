83174e8bb35dacdd41785d224637efd561af4b0e
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 22:57:07 +0300

    Update pop_func.c

--- pop_func.c
+++ pop_func.c
@@ -1,27 +1,23 @@
 #include "monty.h"
-
 /**
- * pop_top - removes the top element from the stack
- * @stack_head: pointer to the head of the stack
- * @line_number: line number in the script
- * Return: 0 on success, -1 on failure
- */
-int pop_top(stack_t **stack_head, unsigned int line_number)
+ * f_pop - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_pop(stack_t **head, unsigned int counter)
 {
-    stack_t *temp;
+	stack_t *h;
 
-    if (*stack_head == NULL)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*stack_head);
-        return -1; // Return an error code
-    }
-
-    temp = *stack_head;
-    *stack_head = temp->next;
-    free(temp);
-
-    return 0; // Success
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
 }
