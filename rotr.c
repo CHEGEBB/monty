8bc3077799d7f7065260b456024cd082dfe22756
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 22:15:29 +0300

    Update rotr.c

--- rotr.c
+++ rotr.c
@@ -1,1 +1,27 @@
-s
+#include "monty.h"
+
+/**
+ * f_rotr - Rotates the stack to the bottom.
+ * @head: Double pointer to the beginning of the stack.
+ * @counter: Line number in the Monty file.
+ */
+void f_rotr(stack_t **head, unsigned int counter)
+{
+	stack_t *temp;
+
+	if (head && *head && (*head)->next)
+	{
+		temp = *head;
+
+		while (temp->next)
+			temp = temp->next;
+
+		temp->prev->next = NULL;
+		temp->prev = NULL;
+		temp->next = *head;
+		(*head)->prev = temp;
+		*head = temp;
+	}
+
+	(void)counter;
+}
