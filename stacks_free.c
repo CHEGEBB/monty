6aeb8fb3210805527d785d79ca6bde26896ed5d3
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 19:59:28 +0300

    Update stacks_free.c

--- stacks_free.c
+++ stacks_free.c
@@ -11,15 +11,15 @@
  */
 void free_stack(stack_t *head)
 {
-    stack_t *current;  // Pointer to the current node being freed
+    stack_t *current;  /* Pointer to the current node being freed */
 
-    current = head;  // Initialize the current pointer to the head
+    current = head;  /* Initialize the current pointer to the head */
 
-    // Iterate through the doubly linked list
+    /* Iterate through the doubly linked list */
     while (head)
     {
-        current = head->next;  // Move to the next node
-        free(head);  // Free the current node
-        head = current;  // Update the head to the next node
+        current = head->next;  /* Move to the next node */
+        free(head);  /* Free the current node */
+        head = current;  /* Update the head to the next node */
     }
 }
