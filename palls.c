0d0e64375e72cdaa5dcb698e531345072ead58cd
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 19:21:08 +0300

    Update palls.c

--- palls.c
+++ palls.c
@@ -1,21 +1,28 @@
 #include "monty.h"
-/**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
 
-	h = *head;
-	if (h == NULL)
+/**
+ * f_pall - Prints the stack.
+ * @stack_head: Stack head
+ * @line_number: Not used
+ *
+ * Description:
+ * This function prints all elements in the stack.
+ *
+ * Return: No return value.
+ */
+void f_pall(stack_t **stack_head, unsigned int line_number)
+{
+	stack_t *current;
+	(void) line_number;
+
+	current = *stack_head;
+
+	if (current == NULL)
 		return;
-	while (h)
+
+	while (current)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", current->n);
+		current = current->next;
 	}
 }
