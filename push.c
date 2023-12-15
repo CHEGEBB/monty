adb8a60f724fe9a8cac4e0be2a18162c204e6b97
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 21:20:52 +0300

    Update push.c

--- push.c
+++ push.c
@@ -1,36 +1,35 @@
 #include "monty.h"
 
 /**
- * push_value - Pushes an element onto the stack.
- * @value: The value to push onto the stack.
+ * f_push - Pushes an element onto the stack.
  * @stack: Double pointer to the stack.
  * @line_number: Line number in the Monty byte code file.
  */
-void push_value(char *value, stack_t **stack, unsigned int line_number)
+void f_push(stack_t **stack, unsigned int line_number)
 {
-    int num;
+	int num;
 
-    if (!value || !isdigit(*value) && *value != '-')
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (!bus.arg || (!isdigit(*bus.arg) && *bus.arg != '-'))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    num = atoi(value);
+	num = atoi(bus.arg);
 
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	stack_t *new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = num;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	new_node->n = num;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    if (*stack)
-        (*stack)->prev = new_node;
+	if (*stack)
+		(*stack)->prev = new_node;
 
-    *stack = new_node;
+	*stack = new_node;
 }
