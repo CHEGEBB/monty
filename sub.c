8e3f944442880614f7d401c83f3636117ab0506f
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 20:03:14 +0300

    Update sub.c

--- sub.c
+++ sub.c
@@ -8,18 +8,22 @@
 */
 void pop_top(stack_t **stack_head, unsigned int line_number)
 {
-	stack_t *temp;
+    stack_t *temp;
 
-	if (*stack_head == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*stack_head);
-		exit(EXIT_FAILURE);
-	}
+    if (*stack_head == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*stack_head);
+        exit(EXIT_FAILURE);
+    }
 
-	temp = *stack_head;
-	*stack_head = temp->next;
-	free(temp);
+    temp = *stack_head;
+    *stack_head = temp->next;
+    if (*stack_head)
+    {
+        (*stack_head)->prev = NULL;
+    }
+    free(temp);
 }
