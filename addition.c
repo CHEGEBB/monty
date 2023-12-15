00e19f443179bd02817eb9425f9dc51a6925d86d
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-15 18:54:30 +0300

    Update addition.c

--- addition.c
+++ addition.c
@@ -1,32 +1,43 @@
 #include "monty.h"
+
 /**
- * f_add - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+ * f_add - Adds the top two elements of the stack.
+ * @head: Pointer to the stack head
+ * @counter: Line number in the Monty file
+ *
+ * Description: This function adds the values of the top two elements of the
+ * stack and replaces the second element with the result.
+ *
+ * Return: No return value
+ */
 void f_add(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	int len = 0, aux;
+    stack_t *h;
+    int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	aux = h->n + h->next->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+    h = *head;
+
+    /* Count the number of elements in the stack */
+    while (h)
+    {
+        h = h->next;
+        len++;
+    }
+
+    /* Check if there are at least two elements in the stack */
+    if (len < 2)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Retrieve the top two elements, add them, and update the stack */
+    h = *head;
+    aux = h->n + h->next->n;
+    h->next->n = aux;
+    *head = h->next;
+    free(h);
 }
