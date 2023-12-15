8455e6847f50b5a69a3e2bbec5c87ed48e912eeb
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-16 00:16:28 +0300

    Create comments.c

---
+++ comments.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_comments - Does nothing for comments.
+ * @head: Double pointer to the beginning of the stack.
+ * @counter: Line number in the Monty file.
+ */
+void f_comments(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	/* Do nothing for comments */
+}
